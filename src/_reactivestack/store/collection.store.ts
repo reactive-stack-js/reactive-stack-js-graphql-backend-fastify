d0455d828fe9b194a6775a805409bffa83054a07
predrags <predrags@nvidia.com>
2021-02-04 11:19:09 +0100

    adding CountStore

--- src/_reactivestack/store/collection.store.ts
+++ src/_reactivestack/store/collection.store.ts
@@ -8,8 +8,8 @@ import AStore, {EStoreType} from './_a.store';
 import observableModel from '../util/_f.observable.model';
 
 export default class CollectionStore extends AStore {
-	constructor(model: Model<any>, field: string) {
-		super(model, field);
+	constructor(model: Model<any>, target: string) {
+		super(model, target);
 		this._type = EStoreType.COLLECTION;
 		Object.setPrototypeOf(this, CollectionStore.prototype);
 	}
@@ -22,15 +22,13 @@ export default class CollectionStore extends AStore {
 	}
 
 	protected async load(): Promise<void> {
-		// console.log(" - CollectionSection load", this._field, this._query, this._sort, this._fields, this._paging);
+		// console.log(" - CollectionStore load", this._field, this._query, this._sort, this._fields, this._paging);
 		if (isEmpty(this._config)) return this.emit();
 
 		let data = [];
 		const total = await this._model.countDocuments(this._query);
 
-		if (total > 0) {
-			data = await this._model.find(this._query, this._fields, this._paging).sort(this._sort);
-		}
+		if (total > 0) data = await this._model.find(this._query, this._fields, this._paging).sort(this._sort);
 
 		this.emit({total, data});
 	}
