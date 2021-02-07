e4fe869b6a29201ebbbb1c06995613973f0453af
predrags <predrags@nvidia.com>
2021-02-07 11:16:52 +0100

    refactoring

--- src/server.ts
+++ src/server.ts
@@ -58,7 +58,16 @@ server.register(fastifyJwt, {secret: process.env.JWT_SECRET});
 server.register(fastifyHelmet, {contentSecurityPolicy: false}); // NOTE: do this only on NON-PROD environments!
 server.register(fastifyCors, {
 	// put your options here
-	origin: ['http://localhost:3007', 'http://localhost:3008']
+	origin: [
+		'http://localhost:3001',
+		'http://localhost:3002',
+		'http://localhost:3003',
+		'http://localhost:3004',
+		'http://localhost:3005',
+		'http://localhost:3006',
+		'http://localhost:3007',
+		'http://localhost:3008'
+	]
 });
 
 const _addJWTHook = (srv: FastifyInstance<Server, IncomingMessage, ServerResponse>): void => {
